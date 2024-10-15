ba6364d75113d6cabf4fa70ddad011c2a1b61ba7
00ste <amalberti75@gmail.com>
2024-10-15 16:54:43 +0200

    main.cpp and complete test

--- Makefile
+++ Makefile
@@ -1,32 +1,12 @@
-# Compiler
-CC = gcc
+CPP_SOURCES = $(wildcard src/*.cpp)
+CPP_HEADERS = $(wildcard include/*.h)
 
-# Compiler flags
-CFLAGS = -Wall -g
+NAME = fields
 
-# Source files
-SRCS = Matrix2x2.cpp tests/MatrixOperations.cpp
+all: $(NAME)
 
-# Object files
-OBJS = $(SRCS:.c=.o)
+fields: $(CPP_SOURCES) $(CPP_HEADERS)
+	g++ -o $(NAME) $(CPP_SOURCES)
 
-# Executable name
-EXEC = run_experiment
-
-# Default target
-all: $(EXEC)
-
-# Link the object files to create the executable
-$(EXEC): $(OBJS)
-	$(CC) $(CFLAGS) -o $@ $^
-
-# Compile source files into object files
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
-
-# Clean up build files
 clean:
-	rm -f $(OBJS) $(EXEC)
-
-# Phony targets
-.PHONY: all clean
+	rm $(NAME)
---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+#include "../include/Data.h"
+#include "../include/FileInterface.h"
+#include "../include/Field.h"
+
+void printHelp(char* exeName) {
+    std::cout << "Usage: " << exeName << " input-data-file input-points-file output-file" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    InputData data;
+    std::vector<Point> points;
+    std::vector<OutputDataLine> output;
+
+    if (argc < 4) {
+        std::cout << "Not enough parameters provided!" << std::endl;
+        printHelp(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    // Get data from command line args
+    FileInterface::readData(data, argv[1]);
+    FileInterface::readPoints(points, argv[2]);
+
+    output.reserve(points.size());
+
+    // Init Field class
+    Field f = Field{ data };
+
+    for (Point p : points) {
+        // Calculate fields for all points
+        f.calculateEHFields(p.x, p.z);
+
+        // Save output
+        OutputDataLine outputLine;
+        outputLine.Ex = f.E[0];
+        outputLine.Ey = f.E[1];
+        outputLine.Ez = f.E[2];
+        outputLine.Hx = f.H[0];
+        outputLine.Hy = f.H[1];
+        outputLine.Hz = f.H[2];
+        outputLine.xp = p.x;
+        outputLine.zp = p.z;
+
+        output.push_back(outputLine);
+    }
+
+    // Write results into the output file
+    FileInterface::writeData(output, argv[3]);
+
+    return EXIT_SUCCESS;
+}
